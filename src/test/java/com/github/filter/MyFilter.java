45e832c3dfdf241e2b1c6d7ebf063c64f55aff79
doctorwho1986 <doctorwho1986@foxmail.com>
2014-07-26 16:29:34 +0800

    sfffffffffffffffffffffffffffffffffffffa

---
+++ src/test/java/com/github/filter/MyFilter.java
@@ -0,0 +1,48 @@
+package com.github.filter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class MyFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("------init  MyFilter");
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("------doFilter  MyFilter");
+		ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+		Enumeration<String> headerNames = requestAttributes.getRequest().getHeaderNames();
+		HttpServletResponse response2 = (HttpServletResponse) response;
+		while (headerNames.hasMoreElements()) {
+			String string = (String) headerNames.nextElement();
+			System.out.println(string + "----" + requestAttributes.getRequest().getHeader(string));
+			response2.setHeader(string, requestAttributes.getRequest().getHeader(string));
+		}
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("------destroy  MyFilter");
+	}
+
+}
