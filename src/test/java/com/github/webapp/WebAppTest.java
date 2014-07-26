55b53a3d926c1e9c398dbdf29004236ab1677a67
doctorwho1986 <doctorwho1986@foxmail.com>
2014-07-26 16:29:49 +0800

    sfffffffffffffffffffffffffffffffffffffa

---
+++ src/test/java/com/github/webapp/WebAppTest.java
@@ -0,0 +1,28 @@
+package com.github.webapp;
+
+import java.io.File;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+
+public class WebAppTest {
+
+	public static void main(String[] args) throws LifecycleException, ServletException {
+		// TODO Auto-generated method stub
+		String prjectPath = new File("").getAbsolutePath();
+		System.out.println(prjectPath);
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+		tomcat.setHostname("localhost");
+		tomcat.setBaseDir(prjectPath+"/temp");
+
+		Context context = tomcat.addWebapp("/webapp", prjectPath +"/src/test/resources/webapps/webapp");
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+
+}
